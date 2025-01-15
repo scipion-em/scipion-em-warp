28f2a82133c4a5ce06bd6e930a8ade273c0f761d
Yunior C. Fonseca Reyna <cfonsecareyna82@gmail.com>
2025-01-15 13:34:06 +0100

    -  Fix an error creating the command

--- warp/protocols/protocol_motion_ctf.py
+++ warp/protocols/protocol_motion_ctf.py
@@ -158,9 +158,9 @@ class ProtWarpMotionCorr(ProtMovieAlignBase):
             argsDict['--exposure'] = exposure
 
         if extension == '.eer':
-            argsDict['--eer_ngroups'] = exposure
+            argsDict[' --eer_ngroups'] = exposure
             if self.eer_groupexposure.get():
-                argsDict['--eer_groupexposure'] = exposure
+                argsDict[' --eer_groupexposure'] = exposure
 
         cmd = ' '.join(['%s %s' % (k, v) for k, v in argsDict.items()])
         if gainPath:
