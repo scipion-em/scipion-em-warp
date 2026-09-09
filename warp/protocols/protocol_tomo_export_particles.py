07cb9aeeace051448524a532d12f3f7056ae9049
Yunior C. Fonseca Reyna <cfonsecareyna82@gmail.com>
2026-09-09 11:48:51 +0200

    Fix Warp tilt angle convention in particle export

--- warp/protocols/protocol_tomo_export_particles.py
+++ warp/protocols/protocol_tomo_export_particles.py
@@ -282,11 +282,28 @@ class ProtWarpExportParticles(ProtWarpBase):
         cmd = ' '.join(['%s %s' % (k, v) for k, v in argsDict.items()])
         self.runJob(self.getPlugin().getProgram(WARP_TOOLS, TS_IMPORT_ALIGNMENTS), cmd, executable='/bin/bash')
 
+    @staticmethod
+    def _invertTiltAngles(tltPath):
+        with open(tltPath, 'r') as f:
+            angles = [
+                float(line.strip())
+                for line in f
+                if line.strip()
+            ]
+
+        with open(tltPath, 'w') as f:
+            for angle in angles:
+                f.write(f'{-angle}\n')
+
     def createImodFiles(self, ts):
         processingFolder = os.path.abspath(self._getExtraPath(TILTSERIES_FOLDER))
         tiltstackFolder = os.path.join(processingFolder, 'tiltstack', ts.getTsId())
         pwutils.makePath(tiltstackFolder)
+
         ts.writeImodFiles(tiltstackFolder, delimiter=' ', factor=1)
+
+        tltPath = os.path.join(tiltstackFolder, f'{ts.getTsId()}.tlt')
+        self._invertTiltAngles(tltPath)
 
     def cleanIntermediateResults(self):
         self.info(">>> Cleaning intermediate results...")
